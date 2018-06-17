0ab420e0af5f9c0ccf225fc453589bfa3f0a2ea8
Geeth Sandaru Madhushan <geeth.gsm@gmail.com>
2018-06-17 20:48:20 +0530

    report form added

---
+++ Isabella/ReportForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Isabella
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportTabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.reportTabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // reportTabControl
+            // 
+            this.reportTabControl.Controls.Add(this.tabPage1);
+            this.reportTabControl.Controls.Add(this.tabPage2);
+            this.reportTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportTabControl.Location = new System.Drawing.Point(0, 1);
+            this.reportTabControl.Name = "reportTabControl";
+            this.reportTabControl.SelectedIndex = 0;
+            this.reportTabControl.Size = new System.Drawing.Size(801, 448);
+            this.reportTabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(793, 415);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Received";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.LightGray;
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(793, 415);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Issued";
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reportTabControl);
+            this.Name = "ReportForm";
+            this.Text = "Reports";
+            this.reportTabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl reportTabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
