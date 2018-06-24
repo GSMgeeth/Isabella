7defa00dba5fe1a3d9177df9c317a08004e74b95
TechNinjasv2 <gsmmac@yahoo.com>
2018-06-24 23:27:00 +0530

    Revert "checking push"
    This reverts commit 972b4058f3150ca5aaa87390db4f07596e071524.

--- Isabella/Form1.cs
+++ Isabella/Form1.cs
@@ -287,7 +287,7 @@ namespace Isabella
                     "on b.deptNo=d.deptNo " +
                     "where b.date='" + date.ToString("yyyy/M/d") + "'";
             
-            Object tmpDeptNameObj = DeptCmb.SelectedItem;   //checking push
+            Object tmpDeptNameObj = DeptCmb.SelectedItem;
             string tmpBagNo = bagNoTxtBox.Text;
 
             if ((tmpDeptNameObj == null) && (tmpBagNo.Equals("")))
